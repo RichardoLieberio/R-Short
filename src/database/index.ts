864eba87dd88dfcd6221b6c44670286f63a75881
Richardo Lieberio <richardo.alt@gmail.com>
2025-02-13 16:37:19 +0700

    Updated User table schema

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,5 @@
 import { drizzle, NeonHttpDatabase } from 'drizzle-orm/neon-http';
 
 export const db: NeonHttpDatabase<Record<string, never>> = drizzle(process.env.DATABASE_URL!);
+
+export { User } from './schema';
--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,14 +1,17 @@
 /* eslint-disable @typescript-eslint/typedef */
 
-import { integer, pgTable, varchar, text, boolean, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, integer, pgEnum, varchar, boolean, timestamp } from 'drizzle-orm/pg-core';
+
+export const userRoleEnum = pgEnum('user_role', [ 'user', 'admin' ]);
 
 export const User = pgTable('users', {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    name: varchar({ length: 255 }).notNull(),
+    clerk_id: varchar({ length: 50 }).notNull().unique(),
     email: varchar({ length: 255 }).notNull().unique(),
-    password: text().notNull(),
-    coin: integer().notNull().default(0),
+    role: userRoleEnum().notNull().default('user'),
+    coin: integer().notNull().default(3),
     claim_free_coin: boolean().notNull().default(false),
     created_at: timestamp().notNull().defaultNow(),
-    updated_at: timestamp().notNull().defaultNow(),
+    is_deleted: boolean().notNull().default(false),
+    deleted_at: timestamp(),
 });
