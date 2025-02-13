e378f92b87e899227c92758c99a6be1548f60ec6
Richardo Lieberio <richardo.alt@gmail.com>
2025-02-13 08:34:07 +0700

    Installed shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 export default function RootLayout({ children }: { children: ReactNode }): JSX.Element {
     return (
         <html>
-            <body>
+            <body className="text-base">
                 { children }
             </body>
         </html>
