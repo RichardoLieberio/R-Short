2b3ed3a36897fee4a539912371150d7effeef059
Richardo Lieberio <richardo.alt@gmail.com>
2025-02-24 16:09:23 +0700

    Improved ClerkAuth registered handler and removed role checker

--- src/app/_components/ClerkAuth/index.tsx
+++ src/app/_components/ClerkAuth/index.tsx
@@ -1,31 +1,32 @@
 import { ReactNode, JSX } from 'react';
-import { auth, clerkClient, User as ClerkUser } from '@clerk/nextjs/server';
+import { ClerkMiddlewareAuthObject, auth, clerkClient, User as ClerkUser } from '@clerk/nextjs/server';
 import { ClerkClient } from '@clerk/backend';
 import { db, User, Log, logs } from '@database';
 
 export default async function ClerkAuth({ children }: { children: ReactNode }): Promise<JSX.Element> {
-    const { userId }: { userId: string | null } = await auth();
+    const { userId, sessionClaims }: ClerkMiddlewareAuthObject = await auth();
 
-    if (userId) {
+    type SessionClaims = { metadata?: { registered?: boolean } };
+    const claims: SessionClaims = sessionClaims as SessionClaims;
+
+    if (userId && !claims.metadata?.registered) {
         const client: ClerkClient = await clerkClient();
         const user: ClerkUser = await client.users.getUser(userId);
+        const email: string = user.emailAddresses[0].emailAddress;
 
-        if (!user.privateMetadata.registered) {
-            const email: string = user.emailAddresses[0].emailAddress;
+        await db.transaction(async (tx) => {
+            await Promise.all([
+                tx.insert(User)
+                    .values({ clerk_id: userId, email })
+                    .onConflictDoUpdate({ target: User.email, set: { clerk_id: userId } }),
+                tx.insert(Log).values({ email, action: 'user_created', message: logs['user_created']() }),
+            ]);
+        });
 
-            await db.transaction(async (tx) => {
-                await Promise.all([
-                    tx.insert(User)
-                        .values({ clerk_id: userId, email })
-                        .onConflictDoUpdate({ target: User.email, set: { clerk_id: userId, is_deleted: false, deleted_at: null } }),
-                    tx.insert(Log).values({ email, action: 'user_created', message: logs['user_created']() }),
-                ]);
-            });
-
-            await client.users.updateUserMetadata(userId, {
-                privateMetadata: { registered: true },
-            });
-        }
+        await client.users.updateUserMetadata(userId, {
+            publicMetadata: { role: 'user', registered: true },
+            privateMetadata: { registered: true },
+        });
     }
 
     return (
