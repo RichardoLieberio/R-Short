05c7efad5f1f92254e6a5c0e0386e672759f5add
Richardo Lieberio <richardo.alt@gmail.com>
2025-02-24 15:01:52 +0700

    Deleted util folder

--- src/utils/isAdmin.ts
+++
@@ -1,8 +0,0 @@
-import { ClerkMiddlewareAuthObject, auth } from '@clerk/nextjs/server';
-
-export default async function isAdmin(): Promise<boolean> {
-    const { sessionClaims }: ClerkMiddlewareAuthObject = await auth();
-    type SessionClaims = { metadata?: { role?: string } };
-    const claims: SessionClaims = sessionClaims as SessionClaims;
-    return claims.metadata?.role === 'admin';
-}
