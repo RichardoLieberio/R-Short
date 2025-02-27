e2480a4401d97a6cdc289400a96ad1bb7c92c43a
Richardo Lieberio <richardo.alt@gmail.com>
2025-02-27 16:58:45 +0700

    Added custom not found page

---
+++ src/app/(root)/not-found.tsx
@@ -0,0 +1,8 @@
+import { JSX } from 'react';
+import NotFound from '../not-found';
+
+export default function RootNotFound(): JSX.Element {
+    return (
+        <NotFound />
+    );
+};
---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import { JSX } from 'react';
+
+export default function NotFound(): JSX.Element {
+    return (
+        <span className="absolute left-1/2 top-1/2 -translate-x-1/2 ytranslate-y-1/2 flex items-center gap-2">
+            <h1 className="text-lg md:text-xl font-semibold">404</h1>
+            <h2 className="text-sm md:text-base">Page not found</h2>
+        </span>
+    );
+}
