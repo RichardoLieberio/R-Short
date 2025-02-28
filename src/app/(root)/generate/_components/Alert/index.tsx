5aceed52c3e95439cb7f87ddab0b82d5e335793f
Richardo Lieberio <richardo.alt@gmail.com>
2025-02-28 16:35:26 +0700

    Changed loading message

--- src/app/(root)/generate/_components/Alert/index.tsx
+++ src/app/(root)/generate/_components/Alert/index.tsx
@@ -11,15 +11,15 @@ export default function Alert({ status, setStatus }: { status: string, setStatus
     if (status === 'loading') {
         return (
             <AlertDialogContent
-                title={ <span>Generating{ dot }</span> }
-                description={ <span>Creating your content, this won&apos;t take long</span> }
+                title={ <span>Just a moment{ dot }</span> }
+                description={ <span>We&apos;re getting things ready for you</span> }
             />
         );
     } else if (status === 'done') {
         return (
             <AlertDialogContent
                 title={ <span className="flex items-center gap-2"><MdOutlineCheckCircleOutline /> Success</span> }
-                description={ <span>Your content has been generated</span> }
+                description={ <span>Your request has been received</span> }
                 footer={ <Link href="/video"><Button>Go to video</Button></Link> }
             />
         );
