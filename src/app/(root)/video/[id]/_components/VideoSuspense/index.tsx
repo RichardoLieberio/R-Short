19286a7d0881501398fc8d791b6f59cab0ba1c90
Richardo Lieberio <richardo.alt@gmail.com>
2025-02-28 13:11:11 +0700

    Added margin for video

--- src/app/(root)/video/[id]/_components/VideoSuspense/index.tsx
+++ src/app/(root)/video/[id]/_components/VideoSuspense/index.tsx
@@ -5,13 +5,13 @@ import VideoPlayer from '../../../_components/VideoPlayer';
 import { Popover, PopoverContent, PopoverTrigger } from '@components/shadcn/popover';
 import { IoMdInformationCircleOutline } from 'react-icons/io';
 import VideoDetail from '../../../_components/VideoDetail';
-import ActionButton from './../../_components/ActionButton';
+import ActionButton from '../ActionButton';
 
 export default async function VideoSuspense({ id }: { id: number | null }): Promise<JSX.Element> {
     const video: VideoType | undefined | null = id ? await getVideo(+id) : null;
 
     return (
-        <div className="w-fit h-fit absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col lg:flex-row gap-4 lg:gap-16">
+        <div className="w-fit h-fit mt-8 lg:mt-0 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col lg:flex-row gap-4 lg:gap-16">
             <main className="relative">
                 <VideoPlayer videoNotFound={video === undefined} video={video ? video : null} small={true} />
                 <Popover>
