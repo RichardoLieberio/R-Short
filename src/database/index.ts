7867907c0b1a932b737499842044c9429192197e
Richardo Lieberio <richardo.alt@gmail.com>
2025-02-28 15:15:19 +0700

    Added video status for background generating

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,7 +4,7 @@ import ws from 'ws';
 
 neonConfig.webSocketConstructor = ws;
 
-const pool: Pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool: Pool = new Pool({ connectionString: process.env.DATABASE_URI });
 export const db: NeonDatabase<Record<string, never>> = drizzle({ client: pool });
 
 export { Package, User, Video, Transaction } from './schema';
--- src/database/schema.ts
+++ src/database/schema.ts
@@ -3,6 +3,7 @@
 import { pgTable, integer, pgEnum, varchar, json, timestamp } from 'drizzle-orm/pg-core';
 
 export const userRoleEnum = pgEnum('user_role', [ 'user', 'admin' ]);
+export const videoStatusEnum = pgEnum('status', [ 'pending', 'created' ]);
 export const videoDurationEnum = pgEnum('duration', [ '15', '30', '60' ]);
 export const transactionTypeEnum = pgEnum('type', [ 'global', 'local' ]);
 
@@ -25,10 +26,11 @@ export const User = pgTable('users', {
 export const Video = pgTable('videos', {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     user_id: integer().notNull().references(() => User.id),
+    status: videoStatusEnum().notNull().default('pending'),
     style: varchar({ length: 30 }).notNull(),
     duration: videoDurationEnum().notNull(),
     storyboard: varchar({ length: 255 }).notNull(),
-    folder: varchar({ length: 50 }).notNull().unique(),
+    folder: varchar({ length: 50 }).unique(),
     audio_uri: json(),
     image_uri: json(),
     captions: json(),
