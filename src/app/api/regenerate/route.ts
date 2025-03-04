0e3edbce7b4ee62291641606837c9dbf97c3712b
Richardo Lieberio <richardo.alt@gmail.com>
2025-03-04 09:12:54 +0700

    Removed unused User.email and created /api/regenerate

--- src/app/api/regenerate/route.ts
+++ src/app/api/regenerate/route.ts
@@ -1,16 +1,9 @@
 import { NextResponse } from 'next/server';
-import { eq, and, sql } from 'drizzle-orm';
-
+import { auth } from '@clerk/nextjs/server';
 import { Socket } from 'socket.io-client';
 import { getSocket } from '@lib/socket';
-
-import { auth } from '@clerk/nextjs/server';
-
-import { z } from 'zod';
-import formSchema from '@schema/formSchema';
-
+import { eq, and } from 'drizzle-orm';
 import { db, User, Video } from '@database';
-import { QueryResult } from '@neondatabase/serverless';
 
 export async function POST(req: Request): Promise<NextResponse> {
     try {
@@ -28,35 +21,27 @@ export async function POST(req: Request): Promise<NextResponse> {
         if (!user) return NextResponse.json({ message: 'Something went wrong' }, { status: 400 });
 
         if (user.role === 'admin' || user.coin > 0) {
-            const body: { style: unknown, duration: unknown, storyboard: unknown } = await req.json();
-            const { style, duration, storyboard }: { style: string, duration: '15' | '30' | '60', storyboard: string } = formSchema.parse(body);
+            const { videoId }: { videoId: number } = await req.json();
+            const video: { style: string, duration: '15' | '30' | '60', storyboard: string } | undefined = await db.select({ style: Video.style, duration: Video.duration, storyboard: Video.storyboard })
+                .from(Video)
+                .innerJoin(User, eq(User.id, Video.user_id))
+                .where(and(eq(Video.id, videoId), eq(User.clerk_id, userId)))
+                .limit(1)
+                .then((videos) => videos[0]);
 
-            const insertedId: number = await db.transaction(async (tx) => {
-                const [ [ { insertedId } ] ]: [ { insertedId: number }[], QueryResult<never> ] = await Promise.all([
-                    tx.insert(Video)
-                        .values({ user_id: user.id, style, duration, storyboard })
-                        .returning({ insertedId: Video.id }),
-                    tx.update(User)
-                        .set({ coin: sql`${User.coin} - 1` })
-                        .where(and(eq(User.id, user.id), eq(User.role, 'user'))),
-                ]);
-
-                return insertedId;
-            });
+            if (!video) return NextResponse.json({ message: 'Video not found' }, { status: 404 });
 
             fetch(process.env.NEXT_PUBLIC_SERVICE_URI! + '/generate', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ userId, insertedId, style, duration, storyboard }),
+                body: JSON.stringify({ userId, insertedId: videoId, ...video }),
             });
 
-            return NextResponse.json({ message: 'Success', id: insertedId }, { status: 200 });
+            return NextResponse.json({ message: 'Success', id: videoId }, { status: 200 });
         }
 
         return NextResponse.json({ message: "You don't have any coins" }, { status: 400 });
     } catch (error) {
-        if (error instanceof z.ZodError) return NextResponse.json({ errors: error.flatten().fieldErrors }, { status: 429 });
-
         console.error(error);
         return NextResponse.json({ message: (error as Error).message ?? 'Something went wrong' }, { status: 400 });
     }
