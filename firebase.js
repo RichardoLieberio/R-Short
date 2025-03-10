689616e78d25f82810c83aeba5e819c22d3d0786
Richardo Lieberio <richardo.alt@gmail.com>
2025-03-10 10:35:31 +0700

    Firebase will export storage instead of configuration

--- firebase.js
+++ firebase.js
@@ -1,4 +1,7 @@
-export default {
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     projectId: process.env.FIREBASE_PROJECT_ID,
@@ -7,3 +10,8 @@ export default {
     appId: process.env.FIREBASE_APP_ID,
     measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+export default storage;
