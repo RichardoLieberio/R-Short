56ebd3db6fc7f9b45da668298582a77995bd4810
Richardo Lieberio <richardo.alt@gmail.com>
2025-03-11 12:06:52 +0700

    Removed regenerate "pending" video

--- src/app/_components/VideoRenderer/action.ts
+++ src/app/_components/VideoRenderer/action.ts
@@ -1,33 +1,19 @@
 'use server';
 
-import { and, or, eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
 import { db, User, Video } from '@database';
 
 export async function backgroundRender(userId: string): Promise<void> {
-    type videoType = {
-        videoId: number;
-        status: 'pending' | 'generated' | 'created' | 'failed';
-        style: string;
-        duration: string;
-        storyboard: string;
-    };
-
-    const videos: videoType[] = await db.select({ videoId: Video.id, status: Video.status, style: Video.style, duration: Video.duration, storyboard: Video.storyboard })
+    const videos: { videoId: number }[] = await db.select({ videoId: Video.id })
         .from(Video)
         .innerJoin(User, eq(User.id, Video.user_id))
-        .where(and(or(eq(Video.status, 'created'), eq(Video.status, 'pending')), eq(User.clerk_id, userId)));
+        .where(and(eq(Video.status, 'created'), eq(User.clerk_id, userId)));
 
-    videos.forEach(({ videoId, status, style, duration, storyboard }: videoType) => {
-        if (status === 'created') fetch(process.env.NEXT_PUBLIC_SERVICE_URI!, {
+    videos.forEach(({ videoId }: { videoId: number }) => {
+        fetch(process.env.NEXT_PUBLIC_SERVICE_URI!, {
             method: 'PATCH',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ userId, videoId }),
-        });
-
-        if (status === 'pending') fetch(process.env.NEXT_PUBLIC_SERVICE_URI!, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ userId, insertedId: videoId, style, duration, storyboard }),
         });
     });
 }
