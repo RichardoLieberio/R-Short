90d6d2beeeb5f66a9c4821459228446af4daa84d
Richardo Lieberio <richardo.alt@gmail.com>
2025-03-13 14:33:46 +0700

    Added websocket listener for realtime user coin

--- src/app/(root)/_components/Header/_components/Coin/hooks.ts
+++ src/app/(root)/_components/Header/_components/Coin/hooks.ts
@@ -1,11 +1,23 @@
 import { useEffect } from 'react';
+import { Socket } from 'socket.io-client';
+import { useSocket } from '@components/SocketProvider';
 import { useAppSelector, AppDispatch, useAppDispatch } from '@store';
 import { setCoin, setOpenShop } from '@store/user';
 import { getUserCoin } from './action';
 
 export function useCoin(): number | null {
+    const { socket }: { socket: Socket | null } = useSocket();
+
     const coin: number | null = useAppSelector((state) => state.user.coin);
     const dispatch: AppDispatch = useAppDispatch();
+
+    useEffect(() => {
+        if (socket) {
+            socket.on('coin:update', ({ coin }: { coin: number }) => {
+                dispatch(setCoin(coin));
+            });
+        }
+    }, [ socket, dispatch ]);
 
     useEffect(() => {
         async function fetchCoin(): Promise<void> {
