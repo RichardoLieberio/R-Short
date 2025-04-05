a39e219263c43bff5d4517b48f2546c6c6b9e674
Richardo Lieberio <richardo.alt@gmail.com>
2025-04-05 13:57:42 +0700

    Moved Audio outside AbsoluteFill

--- src/components/Video/_components/Scene/index.tsx
+++ src/components/Video/_components/Scene/index.tsx
@@ -12,23 +12,25 @@ type SceneProps = {
 
 export default function Scene({ fontSize, scale, imageUri, audioUri, captions }: SceneProps): JSX.Element {
     return (
-        <AbsoluteFill>
-            <Img src={imageUri} pauseWhenLoading style={{ objectFit: 'cover', width: '100%', height: '100%', transform: `scale(${scale})`, backgroundColor: 'black' }} />
+        <>
             <Audio src={audioUri} pauseWhenBuffering />
-            {
-                captions.map((caption, idx) => {
-                    const startTime: number = Math.floor(((caption.start + 70) / 1000) * 30);
-                    const duration: number = Math.floor(((caption.end - caption.start) / 1000) * 30);
+            <AbsoluteFill>
+                <Img src={imageUri} pauseWhenLoading style={{ objectFit: 'cover', width: '100%', height: '100%', transform: `scale(${scale})`, backgroundColor: 'black' }} />
+                {
+                    captions.map((caption, idx) => {
+                        const startTime: number = Math.floor(((caption.start + 70) / 1000) * 30);
+                        const duration: number = Math.floor(((caption.end - caption.start) / 1000) * 30);
 
-                    return (
-                        <Sequence key={idx} from={startTime} durationInFrames={duration}>
-                            <div style={{ width: '100%', position: 'absolute', bottom: '20%', color: 'white', textAlign: 'center', fontSize: fontSize + 'px', textShadow: '2px 2px 2px rgba(0, 0, 0, 1)' }}>
-                                {caption.text}
-                            </div>
-                        </Sequence>
-                    );
-                })
-            }
-        </AbsoluteFill>
+                        return (
+                            <Sequence key={idx} from={startTime} durationInFrames={duration}>
+                                <div style={{ width: '100%', position: 'absolute', bottom: '20%', color: 'white', textAlign: 'center', fontSize: fontSize + 'px', textShadow: '2px 2px 2px rgba(0, 0, 0, 1)' }}>
+                                    {caption.text}
+                                </div>
+                            </Sequence>
+                        );
+                    })
+                }
+            </AbsoluteFill>
+        </>
     );
 }
